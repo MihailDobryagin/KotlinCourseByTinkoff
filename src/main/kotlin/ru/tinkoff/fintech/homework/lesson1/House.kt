37614895f69eae175898d2848bb770f66cacc26d
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2022-02-19 18:54:13 +0300

    Solve problem with owner's getter and setter

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson1/House.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson1/House.kt
@@ -5,18 +5,17 @@ import ru.tinkoff.fintech.homework.lesson1.animals.Human
 import ru.tinkoff.fintech.homework.lesson1.animals.SomeoneWhoCanHaveHome
 
 class House(owner: Human) {
-    private lateinit var owner: Human
+    var owner: Human = owner
+        set(value) {
+            field = value
+            println("$field has own house")
+        }
 
     // У этих полей будут свои геттеры, т. к.
-    //  1) Нужно будет возвращать List, а не MutableList
+    //  1) Нужно будет возвращать Set, а не MutableSet
     //  2) При get из самого House нам нужен именно сам объект residents, а не его копия
     private val residents: MutableSet<SomeoneWhoCanHaveHome> = mutableSetOf()
     private val pets: MutableSet<DomesticatedAnimal> = mutableSetOf()
-
-    init {
-        setOwner(owner)
-        addResident(owner)
-    }
 
     fun getResidents() = residents.toSet()
     fun getPets() = pets.toSet()
@@ -26,25 +25,21 @@ class House(owner: Human) {
         resident.moveIntoHouse(this)
     }
 
+    fun evictResident(resident: SomeoneWhoCanHaveHome) {
+        resident.evict()
+        residents.remove(resident)
+        pets.remove(resident)
+        println("$resident left home")
+    }
+
     fun addPet(pet: DomesticatedAnimal) {
         addResident(pet)
-        pets.add(pet)
         println("$pet was added as pet")
     }
 
     fun addPets(animals: Iterable<DomesticatedAnimal>) = animals.forEach(this::addPet)
 
-    fun deletePet(pet: DomesticatedAnimal) {
-        pets.remove(pet)
-        residents.remove(pet)
-        println("$pet left home")
-    }
-
-    fun getOwner() = owner
-    fun setOwner(owner: Human) {
-        this.owner = owner
-        println("$owner has own house")
-    }
+    fun evictPet(pet: DomesticatedAnimal) = evictResident(pet)
 
     override fun toString(): String {
         return "Residents:\n" +
