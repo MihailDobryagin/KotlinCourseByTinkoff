9229340193da26c27c03142724aadc5c87b1bfa4
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2022-03-08 14:42:01 +0300

    Fix some issues

--- src/test/kotlin/ru/tinkoff/fintech/homework/lesson3/animals/CatTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/lesson3/animals/CatTest.kt
@@ -1,36 +1,38 @@
 package ru.tinkoff.fintech.homework.lesson3.animals
 
-import io.mockk.every
-import io.mockk.spyk
-import io.mockk.verify
+import io.mockk.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
-import kotlin.properties.Delegates
 import kotlin.test.assertEquals
 
 class CatTest {
     private lateinit var cat: Cat
-    private var previousWeight by Delegates.notNull<Int>()
 
     @BeforeEach
     fun beforeEach() {
-        cat = spyk(Cat("Boris", null))
-        previousWeight = cat.weight
+        cat = spyk(Cat("Boris", "белый"))
+    }
+
+    @AfterEach
+    fun afterEach() {
+        clearAllMocks()
     }
 
     @Test
     fun checkEatMouse() {
         val weightDifference = 123
+
         val mouse = spyk<Mouse> {
             every { weight } returns weightDifference
         }
-        val expectedWeight = previousWeight + weightDifference
+
+        val expectedWeight = cat.weight + weightDifference
 
         cat.eatMouse(mouse)
 
         assertAll(
-            null,
             { assert(!mouse.alive) },
             { assertEquals(expectedWeight, cat.weight) },
             { assertEquals(1, cat.countOfEatenMouses) }
@@ -39,15 +41,14 @@ class CatTest {
 
     @Test
     fun checkEatMouseWhenMouseIsDead() {
-        val deadMouse = spyk<Mouse> {
-            every { alive } returns false
-        }
+        val deadMouse = mockk<Mouse>()
+        every { deadMouse.alive } returns false
+
         val expectedWeight = cat.weight
 
         cat.eatMouse(deadMouse)
 
         assertAll(
-            null,
             { verify(exactly = 0) { cat.eat(any()) } },
             { assertEquals(expectedWeight, cat.weight) },
         )
--- src/test/kotlin/ru/tinkoff/fintech/homework/lesson3/animals/HouseTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/lesson3/animals/HouseTest.kt
@@ -1,7 +1,9 @@
 package ru.tinkoff.fintech.homework.lesson3.animals
 
+import io.mockk.clearAllMocks
 import io.mockk.spyk
 import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
@@ -18,13 +20,17 @@ class HouseTest {
     fun beforeEach() {
         human = spyk(Human("MainHuman", 80))
         house = House(human)
-        cat = spyk(Cat("BobCat", null))
+        cat = spyk(Cat("BobCat", "white"))
+    }
+
+    @AfterEach
+    fun afterEach() {
+        clearAllMocks()
     }
 
     @Test
     fun checkInitialization() {
         assertAll(
-            null,
             { assert(isHumanInHouse(house, human)) },
             { assertContentEquals(setOf(human), house.getResidents().asIterable()) },
             { verify { house.addResident(human) } }
@@ -36,7 +42,6 @@ class HouseTest {
         val newHuman = spyk(Human("NewHuman", 111))
         house.owner = newHuman
         assertAll(
-            null,
             { assert(isHumanInHouse(house, human)) },
             { assert(house.getResidents().containsAll(setOf(human, newHuman))) }
         )
@@ -54,7 +59,6 @@ class HouseTest {
         house.addPet(cat)
 
         assertAll(
-            null,
             { assert(house.getResidents().containsAll(listOf(cat, human))) },
             { assertContentEquals(setOf(cat).asIterable(), house.getPets()) },
             { verify { house.addPet(cat) } },
@@ -67,7 +71,6 @@ class HouseTest {
         house.evictPet(cat)
 
         assertAll(
-            null,
             { assert(!house.getResidents().union(house.getPets()).contains(cat)) },
             { assertEquals(null, cat.home) },
             { verify { house.evictResident(cat) } },
