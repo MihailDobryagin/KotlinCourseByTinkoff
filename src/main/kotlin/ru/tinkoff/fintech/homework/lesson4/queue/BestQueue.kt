d808b32162e103462c13059ed17815cf168a2fec
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2022-03-12 12:26:04 +0300

    Some changes

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson4/queue/BestQueue.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson4/queue/BestQueue.kt
@@ -1,8 +1,6 @@
 package ru.tinkoff.fintech.homework.lesson4.queue
 
-import java.util.*
-
-class BestQueue<T> : Queue<T> {
+class BestQueue<T> {
 
     private var head: Node<T>? = null
         set(value) {
@@ -17,9 +15,10 @@ class BestQueue<T> : Queue<T> {
 
     private var back: Node<T>? = null
 
-    override var size: Int = 0
+    var size: Int = 0
+        private set
 
-    override fun add(element: T): Boolean {
+    fun add(element: T): Boolean {
         if (size == 0) {
             head = Node(element, null)
             size++
@@ -32,56 +31,32 @@ class BestQueue<T> : Queue<T> {
         return true
     }
 
-    override fun addAll(elements: Collection<T>): Boolean {
+    fun addAll(elements: Collection<T>): Boolean {
         elements.forEach(this::add)
         return true
     }
 
-    override fun clear() {
+    fun clear() {
         head = null
         size = 0
     }
 
-    override fun iterator(): MutableIterator<T> = Iterator(head)
-
-    override fun remove(): T =
+    fun remove(): T =
         poll() ?: throw NoSuchElementException()
 
-    override fun contains(element: T): Boolean {
-        TODO("Not yet implemented")
-    }
+    fun isEmpty(): Boolean = size == 0
 
-    override fun containsAll(elements: Collection<T>): Boolean {
-        TODO("Not yet implemented")
-    }
+    fun offer(e: T): Boolean = add(e)
 
-    override fun isEmpty(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun remove(element: T): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun removeAll(elements: Collection<T>): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun retainAll(elements: Collection<T>): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun offer(e: T): Boolean = add(e)
-
-    override fun poll(): T? {
+    fun poll(): T? {
         if (size == 0) return null
         val result = head!!.value
         head = head!!.next
         return result
     }
 
-    override fun element(): T =
+    fun element(): T =
         head?.value ?: throw NoSuchElementException()
 
-    override fun peek(): T? = head?.value
+    fun peek(): T? = head?.value
 }
