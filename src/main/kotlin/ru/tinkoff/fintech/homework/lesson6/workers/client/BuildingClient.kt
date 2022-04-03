82756634f6c64f898d5759be66c12729eccbeaa3
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2022-04-03 12:08:43 +0300

    Some changes

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson6/workers/client/BuildingClient.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson6/workers/client/BuildingClient.kt
@@ -3,7 +3,6 @@ package ru.tinkoff.fintech.homework.lesson6.workers.client
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import ru.tinkoff.fintech.homework.lesson6.workers.client.dto.MoveWorkerDto
-import ru.tinkoff.fintech.homework.lesson6.workers.client.request.MoveWorkerRequest
 
 @Component
 class BuildingClient(
@@ -13,8 +12,7 @@ class BuildingClient(
 
     fun moveWorker(from: Long?, to: Long?): Boolean {
         val requestDto = MoveWorkerDto(from, to)
-        val request = MoveWorkerRequest(requestDto)
-        val result = restTemplate.postForEntity(PATH + request.uri, request.dto, SimpleResponse::class.java).body
+        val result = restTemplate.postForEntity(PATH + "workers/move", requestDto, SimpleResponse::class.java).body
         return result?.success ?: false
     }
 }
