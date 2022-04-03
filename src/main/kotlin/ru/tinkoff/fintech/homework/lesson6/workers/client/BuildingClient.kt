4c50951c6d4c17d8f902a6745fd535b71ef57c25
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2022-04-03 12:06:05 +0300

    Some changes

--- src/main/kotlin/ru/tinkoff/fintech/homework/lesson6/workers/client/BuildingClient.kt
+++ src/main/kotlin/ru/tinkoff/fintech/homework/lesson6/workers/client/BuildingClient.kt
@@ -1,12 +1,9 @@
 package ru.tinkoff.fintech.homework.lesson6.workers.client
 
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.postForEntity
 import ru.tinkoff.fintech.homework.lesson6.workers.client.dto.MoveWorkerDto
 import ru.tinkoff.fintech.homework.lesson6.workers.client.request.MoveWorkerRequest
-import ru.tinkoff.fintech.homework.lesson6.workers.client.request.RequestTemplate
 
 @Component
 class BuildingClient(
@@ -16,11 +13,8 @@ class BuildingClient(
 
     fun moveWorker(from: Long?, to: Long?): Boolean {
         val requestDto = MoveWorkerDto(from, to)
-        val movingResult = doRequest(MoveWorkerRequest(requestDto)).body as Map<*, *>
-        return movingResult["success"] as Boolean
-    }
-
-    private fun doRequest(request: RequestTemplate): ResponseEntity<Any> {
-        return restTemplate.postForEntity(PATH + request.uri, request.dto)
+        val request = MoveWorkerRequest(requestDto)
+        val result = restTemplate.postForEntity(PATH + request.uri, request.dto, SimpleResponse::class.java).body
+        return result?.success ?: false
     }
 }
